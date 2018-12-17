78694f60046d2bb487bc42fbfb0037c174a9a7f6
talexcrowell <talexcrowell@gmail.com>
2018-12-17 12:34:02 -0800

    palindrome

--- hashmap-drill.js
+++ hashmap-drill.js
@@ -22,4 +22,51 @@ function main(){
   console.log(lor);
   console.log(lor._slots[2]);
 }
-main();
+// main();
+
+/* 
+input: acecarr
+output: true
+
+instance of one unique character
+
+loop through string and add to hash table (key= string[i], value = i)
+a,0
+when duplicate key happens, value is overwritten
+
+total of original string compared to total of hash (works besides non palindrome double letters)
+
+
+*/
+
+function palindrome(str){
+  const palHash = new HashMap();
+  let total = 0;
+ 
+  for(let i=0; i<str.length; i++){
+    let character = str[i];
+    try{
+      let value = palHash.get(character);
+      value++;
+      palHash.set(character, value);
+    }
+    catch (error){
+      palHash.set(character, 1);
+    }
+  }
+  
+  for(let i=0; i<str.length; i++){
+    let check = palHash.get(str[i]);
+    if(check ===1){
+      total++;
+    }
+  }
+
+  if(total > 1){
+    return false;
+  }
+  return true;
+
+}
+
+palindrome('acecarr');
